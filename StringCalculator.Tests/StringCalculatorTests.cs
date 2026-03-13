4e0a92720b0a5860cff5e03694d2d14ce974d1cd
Artem <artem1podderegin@gmail.com>
2026-03-13 13:37:02 +0100

    Initial commit

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Xunit;
+using StringCalculator;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        private readonly StringCalculator _calculator = new StringCalculator();
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData(null, 0)]
+        public void Calculate_EmptyOrNullString_ReturnsZero(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("5", 5)]
+        [InlineData("42", 42)]
+        [InlineData("999", 999)]
+        public void Calculate_SingleNumber_ReturnsThatNumber(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("0,0", 0)]
+        [InlineData("10,20", 30)]
+        [InlineData("100,200", 300)]
+        public void Calculate_TwoNumbersCommaDelimited_ReturnsSum(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1\n2", 3)]
+        [InlineData("10\n20", 30)]
+        [InlineData("0\n5", 5)]
+        public void Calculate_TwoNumbersNewlineDelimited_ReturnsSum(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("1\n2\n3", 6)]
+        [InlineData("1,2\n3", 6)]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("10,20,30", 60)]
+        public void Calculate_ThreeNumbersMixedDelimiters_ReturnsSum(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("-1", "-1")]
+        [InlineData("-1,-2", "-1, -2")]
+        [InlineData("1,-2,3,-4", "-2, -4")]
+        public void Calculate_NegativeNumbers_ThrowsException(
+            string input, string expectedNegatives)
+        {
+            var exception = Assert.Throws<ArgumentException>(
+                () => _calculator.Calculate(input)
+            );
+
+            Assert.Contains(expectedNegatives, exception.Message);
+        }
+
+        [Theory]
+        [InlineData("1001", 0)]
+        [InlineData("2,1001", 2)]
+        [InlineData("1000,1001,2", 1002)]
+        [InlineData("1000,2", 1002)]
+        [InlineData("5000,3,2000,7", 10)]
+        public void Calculate_NumbersGreaterThan1000_AreIgnored(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("//#\n1#2", 3)]
+        [InlineData("//;\n1;2;3", 6)]
+        [InlineData("//|\n10|20|30", 60)]
+        [InlineData("//_\n5_5", 10)]
+        public void Calculate_SingleCharCustomDelimiter_ReturnsSum(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        
+        [Theory]
+        [InlineData("//[###]\n1###2###3", 6)]
+        [InlineData("//[**]\n10**20", 30)]
+        [InlineData("//[abc]\n5abc10abc15", 30)]
+        [InlineData("//[;;]\n100;;200;;300", 600)]
+        public void Calculate_MultiCharCustomDelimiter_ReturnsSum(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("//[#][%]\n1#2%3", 6)]
+        [InlineData("//[##][%%]\n1##2%%3", 6)]
+        [InlineData("//[*][;][|]\n1*2;3|4", 10)]
+        [InlineData("//[ab][cd]\n10ab20cd30", 60)]
+        public void Calculate_ManyCustomDelimiters_ReturnsSum(string input, int expected)
+        {
+            int result = _calculator.Calculate(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Calculate(string arg)
+        {
+            if (string.IsNullOrEmpty(arg))
+                return 0;
+
+            List<string> delimiters = new List<string> { ",", "\n" };
+            string numbersSection = arg;
+
+            if (arg.StartsWith("//"))
+            {
+                int endFirstLine = arg.IndexOf("\n");
+                string delimiterPart = arg.Substring(2, endFirstLine - 2);
+                numbersSection = arg.Substring(endFirstLine + 1);
+
+                delimiters.Clear();
+
+                if (delimiterPart.Contains("["))
+                {
+                    string temp = delimiterPart;
+                    while (temp.Contains("["))
+                    {
+                        int start = temp.IndexOf("[");
+                        int end = temp.IndexOf("]");
+                        string delimiter = temp.Substring(start + 1, end - start - 1);
+                        delimiters.Add(delimiter);
+                        temp = temp.Substring(end + 1);
+                    }
+                }
+                else
+                {
+                    delimiters.Add(delimiterPart);
+                }
+            }
+
+            string[] parts = numbersSection.Split(delimiters.ToArray(), StringSplitOptions.None);
+
+            List<int> numbers = new List<int>();
+            List<int> negatives = new List<int>();
+
+            foreach (string part in parts)
+            {
+                int number = int.Parse(part);
+
+                if (number < 0)
+                {
+                    negatives.Add(number);
+                    continue;
+                }
+
+                if (number > 1000)
+                    continue;
+
+                numbers.Add(number);
+            }
+            
+            if (negatives.Count > 0)
+            {
+                string negativesList = string.Join(", ", negatives);
+                throw new ArgumentException($"Negatives not allowed: {negativesList}");
+            }
+
+            return numbers.Sum();
+        }
+    }
+}
